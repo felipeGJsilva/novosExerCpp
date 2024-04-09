68eaf901e0a23e65d2568af03fe53da299279bcc
FelipeJansen <132519887+felipeGJsilva@users.noreply.github.com>
2024-04-09 00:09:03 +0000

    feito

--- exercicio5.cpp
+++ exercicio5.cpp
@@ -1,1 +1,16 @@
-# include <iostream)
+# include <iostream>
+
+int main(){
+    float n1, n2 ;
+
+    printf("Digite o primeiro número: ");
+    scanf("%f", &n1);
+    
+    printf("Digite o segundo número: ");
+    scanf("%f", &n2);
+    
+    char ope;
+
+    return false;
+
+}
