472991863fe434315c266a57a5bc5560a009a981
FelipeJansen <132519887+felipeGJsilva@users.noreply.github.com>
2024-04-10 00:24:12 +0000

    feito

---
+++ exercicio10.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(){
+
+    float saldo = 100000;
+    float saque ;
+
+    printf("Digite o valor para ser retirado: ");
+    scanf("%f",&saque)
+
+    if (saldo >= saque){
+        printf(" você poode retirar ")
+    }
+    else if (saldo < saque){
+        printf("  Você não pode retirar pois não possui saldo suficiente")
+    }
+}
