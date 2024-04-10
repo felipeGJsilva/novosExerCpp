90c5a9d3c51cf10a35824aa25b383b2c87e71f94
FelipeJansen <132519887+felipeGJsilva@users.noreply.github.com>
2024-04-10 00:35:32 +0000

    FEITO

---
+++ exercicio11.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main(){
+
+    int lead ;
+
+    printf("Digite a quantidade de leads: ");
+    scanf("%d", &lead);
+
+    if ( lead >=6 && lead <= 10 ){
+        printf(" Quantidade esperada \n");
+    }
+
+    else if(lead <=5 ){
+        printf(" Quantidade baixa \n");
+    }
+    else if(lead >=11){
+        printf(" Quantidade Alta \n");
+    }
+
+    
+
+
+    return false;
+}
